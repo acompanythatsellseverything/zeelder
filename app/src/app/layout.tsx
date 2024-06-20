442492d234afcdb6b512810b727c4144bd5fc847
nechamdod <nechamdod@gmail.com>
2024-06-20 13:31:17 +0300

    init project

---
+++ app/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next';
+import { Manrope } from 'next/font/google';
+import './globals.css';
+
+const inter = Manrope({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+	title: 'Zeelder',
+	description:
+		'The fastest provider of online custom laser cutting, bending, welding & digital prototyping services in Europe',
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang='en'>
+			<body className={inter.className}>{children}</body>
+		</html>
+	);
+}
