8be1c917acc72651e05e2924b7f763a0d8c2ec2f
nechamdod <nechamdod@gmail.com>
2024-07-26 15:01:04 +0300

    hot-fix

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -3,7 +3,7 @@ import { GoogleAnalytics } from '@next/third-parties/google';
 import { Manrope } from 'next/font/google';
 import './globals.css';
 import { Providers } from '@/providers/Providers';
-import { GA_ID, MICROSOFT_CLARITY_ID } from '../constants/analytics';
+import { GA_ID, GTM_ID, MICROSOFT_CLARITY_ID } from '../constants/analytics';
 import Script from 'next/script';
 
 const inter = Manrope({ subsets: ['latin'] });
@@ -19,10 +19,36 @@ export default function RootLayout({
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
+	console.log(GA_ID, GTM_ID, MICROSOFT_CLARITY_ID)
 	return (
 		<html lang='en'>
+			<head>
+			<Script
+					id='gtm-config'
+					strategy='beforeInteractive'
+					dangerouslySetInnerHTML={{
+						__html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${GTM_ID}');
+            `,
+					}}
+				/>
+			
+			</head>
 			<body className={inter.className}>
+			<noscript>
+					<iframe
+						src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+						height='0'
+						width='0'
+						style={{ display: 'none', visibility: 'hidden' }}
+					></iframe>
+				</noscript>
 				<Providers>{children}</Providers>
+
 				<GoogleAnalytics gaId={GA_ID} />
 				<Script id='clarity-script' strategy='afterInteractive'>
 					{`
