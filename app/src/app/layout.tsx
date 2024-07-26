fb01f773645c4bad93babb4bfb377fe3d71de892
nechamdod <nechamdod@gmail.com>
2024-07-26 16:22:20 +0300

    feat: clarity

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -44,19 +44,24 @@ export default function RootLayout({
         page_path: window.location.pathname,
       });`}
 				</Script>
+
 				<Script
 					strategy='afterInteractive'
 					src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
 				/>
-				<Script id='clarity-script' strategy='afterInteractive'>
-					{`
-					(function(c,l,a,r,i,t,y){
-						c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-						t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-						y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    			})(window, document, "clarity", "script", "${MICROSOFT_CLARITY_ID}");
-					`}
-				</Script>
+				<Script
+					strategy='afterInteractive'
+					id='clarity-script'
+					dangerouslySetInnerHTML={{
+						__html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "${MICROSOFT_CLARITY_ID}");
+            `,
+					}}
+				/>
 			</head>
 			<body className={inter.className}>
 				<noscript>
