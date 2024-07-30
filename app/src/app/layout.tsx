3e9466b4fe3f75bed9ff8a37598c7ce5d7dca287
nechamdod <nechamdod@gmail.com>
2024-07-30 17:14:08 +0300

    feat: favicon

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
 	return (
 		<html lang='en'>
 			<head>
+				<link rel='icon' href='/favicon.png' sizes='any' />
 				<Script
 					id='gtm-config'
 					strategy='beforeInteractive'
