8e014ddbff4c33740d5f5ea7ad55c785b8fdc85a
nechamdod <nechamdod@gmail.com>
2024-08-02 15:16:06 +0300

    fix: header

--- app/src/components/NavBar/NavBar.tsx
+++ app/src/components/NavBar/NavBar.tsx
@@ -10,7 +10,7 @@ import {
 	NavbarMenuItem,
 	NavbarMenuToggle,
 } from '@nextui-org/react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const navList = [
 	{ title: 'Hardware build', href: '#hardware-build' },
@@ -22,59 +22,83 @@ const navList = [
 
 export default function NavBar() {
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
-	return (
-		<Navbar
-			isMenuOpen={isMenuOpen}
-			onMenuOpenChange={setIsMenuOpen}
-			classNames={{
-				base: 'w-full py-2 border-b-1 border-light',
-				wrapper: 'max-w-none px-4 sm:px-5 md:px-6 lg:px-10 xl:px-32',
-				brand: 'basis-[180px] shrink grow-0',
-				toggle: 'w-10 h-10 rounded-sm bg-accent cursor-pointer',
-			}}
-		>
-			<NavbarBrand>
-				<Link href={'/'} className={'flex items-center'}>
-					{/* <div className={'md:hidden bg-logo-icon w-10 h-10'}/>
-					<div className={'md:hidden bg-accent w-[1px] h-10 mx-2.5'}/> */}
-					<div className={'bg-logo h-[30px] w-[180px] bg-no-repeat'} />
-				</Link>
-			</NavbarBrand>
-			<NavbarContent className={'max-[768px]:hidden'}>
-				<div className='w-full flex items-center justify-end gap-5 lg:gap-16'>
-					<ul className={'basis-full flex justify-center gap-3 lg:gap-6 px-2'}>
-						{navList.map((e) => (
-							<li key={e.title} className={'text-xs lg:text-base'}>
-								<Link href={`/${e.href}`} className='hover:text-red-500'>
-									{e.title}
-								</Link>
-							</li>
-						))}
-					</ul>
-					<div className='shrink-0'>
-						<ArrowButton to={'#'} text={'Get quote'} />
-					</div>
-				</div>
-			</NavbarContent>
-			<div className='md:hidden'>
-				<Burger
-					isOpen={isMenuOpen}
-					toggleOpen={() => setIsMenuOpen((prev) => !prev)}
-				/>
-			</div>
+	useEffect(() => {
+		if(isMenuOpen) {
+			document.body.style.overflow = "hidden";
+			return () => {
+				document.body.style.overflow = "scroll"
+			};
+		} else {
+				document.body.style.overflow = "scroll"
+		}
+	}, [isMenuOpen])
 
-			<NavbarMenu className='gap-0 opacity-100'>
-				{navList.map((item, index) => (
-					<NavbarMenuItem key={`${item}-${index}`} className={`py-4 ${ index != navList.length - 1 && 'border-b-1 border-gray-light'}`}>
-						<Link className='w-full hover:text-red-500' onClick={ () => setIsMenuOpen(false)} href={`/${item.href}`}>
-							{item.title}
-						</Link>
-					</NavbarMenuItem>
-				))}
-				<NavbarMenuItem className='mt-10'>
+	return (
+		<>
+			<Navbar
+				isMenuOpen={isMenuOpen}
+				onMenuOpenChange={setIsMenuOpen}
+				classNames={{
+					base: 'w-full py-2 border-b-1 border-light',
+					wrapper: 'max-w-none px-4 sm:px-5 md:px-6 lg:px-10 xl:px-32',
+					brand: 'basis-[180px] shrink grow-0',
+					toggle: 'w-10 h-10 rounded-sm bg-accent cursor-pointer',
+				}}
+			>
+				<NavbarBrand>
+					<Link href={'/'} className={'flex items-center'}>
+						{/* <div className={'md:hidden bg-logo-icon w-10 h-10'}/>
+					<div className={'md:hidden bg-accent w-[1px] h-10 mx-2.5'}/> */}
+						<div className={'bg-logo h-[30px] w-[180px] bg-no-repeat'} />
+					</Link>
+				</NavbarBrand>
+				<NavbarContent className={'max-[868px]:hidden'}>
+					<div className='w-full flex items-center justify-end gap-5 lg:gap-16'>
+						<ul
+							className={'basis-full flex justify-center gap-3 lg:gap-6 px-2'}
+						>
+							{navList.map((e) => (
+								<li key={e.title} className={'text-xs lg:text-base'}>
+									<Link href={`/${e.href}`} className='hover:text-red-500'>
+										{e.title}
+									</Link>
+								</li>
+							))}
+						</ul>
+						<div className='shrink-0'>
+							<ArrowButton to={'#'} text={'Get quote'} />
+						</div>
+					</div>
+				</NavbarContent>
+				<div className='min-[868px]:hidden'>
+					<Burger
+						isOpen={isMenuOpen}
+						toggleOpen={() => setIsMenuOpen((prev) => !prev)}
+					/>
+				</div>
+			</Navbar>
+			{isMenuOpen && (
+				<div className='absolute px-4 sm:px-5 md:px-6 z-50 bg-white w-full h-full'>
+					{navList.map((item, index) => (
+						<div
+							key={`${item}-${index}`}
+							className={`py-4 ${
+								index != navList.length - 1 && 'border-b-1 border-gray-light'
+							}`}
+						>
+							<Link
+								className='w-full hover:text-red-500'
+								onClick={() => setIsMenuOpen(false)}
+								href={`/${item.href}`}
+							>
+								{item.title}
+							</Link>
+						</div>
+					))}
 					<ArrowButton to={'#'} text={'Get quote'} />
-				</NavbarMenuItem>
-			</NavbarMenu>
-		</Navbar>
+				</div>
+			)}
+
+		</>
 	);
 }
