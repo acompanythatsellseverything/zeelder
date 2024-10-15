f01f5b3ea3069ab8d6286a1fadbc312e4894c5c1
nechamdod <nechamdod@gmail.com>
2024-10-15 12:50:06 +0300

    fix: fix mobile + resolution artifacts

--- app/src/components/Articles/FirstArticle.tsx
+++ app/src/components/Articles/FirstArticle.tsx
@@ -9,25 +9,26 @@ import ArticleScroll from '@/components/ArticleScroll/ArticleScroll';
 export default function FirstArticle() {
 	return (
 		<>
-			<Container
-				className={`relative mt-[50px] md:pb-[80px] bg-article-1-bg bg-no-repeat 
-				bg-mob-right-center md:bg-lap-right-center md:bg-center-right 
-				lg:bg-right-top bg-55 md:bg-60 min-[1424px]:bg-65 `}
+			<div
+				className={`sm:px-5 md:px-6 lg:px-10 xl:px-32 relative mt-[50px] pb-10 md:pb-[120px] bg-none	md:bg-article-1-bg bg-no-repeat md:bg-lap-right-center md:bg-center-right lg:bg-right-top md:bg-60 min-[1424px]:bg-65 `}
 			>
 				<article>
 					<ArticleScroll title='01' coloredLineHeight='8rem' />
 					<div>
-						<ArticleTitle className='max-w-xl'>
-							Order custom parts online, get them manufactured and shipped after
-							24 hours
-						</ArticleTitle>
-						<ArticleSubTitle>
-							The fastest provider of online custom laser cutting, bending,
-							welding & digital prototyping services in Europe
-						</ArticleSubTitle>
+						<div className='px-4 md:px-0'>
+							<ArticleTitle className='max-w-xl'>
+								Order custom parts online, get them manufactured and shipped
+								after 24 hours
+							</ArticleTitle>
 
-						<div className={'mt-5 max-w-md'}>
-							<ul className='grid md:grid-cols-2 grid-cols-1 gap-3'>
+							<ArticleSubTitle>
+								The fastest provider of online custom laser cutting, bending,
+								welding & digital prototyping services in Europe
+							</ArticleSubTitle>
+						</div>
+
+						<div className={'mt-5 md:max-w-md pl-4 md:px-0'}>
+							<ul className='grid sm:grid-cols-2 grid-cols-1 gap-3 bg-article-1-bg bg-no-repeat bg-55 bg-mob-right-center sm:bg-none'>
 								<li
 									className={
 										'flex items-center max-w-[180px] sm:max-w-[142px] md:max-w-200'
@@ -70,15 +71,12 @@ export default function FirstArticle() {
 								</li>
 							</ul>
 						</div>
-						<div className={'mt-5 max-w-2xl hidden md:block'}>
+						<div className={'px-4 md:px-0 mt-5 max-w-2xl'}>
 							<UploadCADForm />
 						</div>
 					</div>
 				</article>
-			</Container>
-			<Container className={'mt-5 max-w-2xl block md:hidden pb-[80px] pt-0'}>
-				<UploadCADForm />
-			</Container>
+			</div>
 		</>
 	);
 }
